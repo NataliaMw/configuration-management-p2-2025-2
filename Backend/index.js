a75ce387f42de7df6a7d97782fd7780bf0fb199f
Steeven Gómez <sjgomez@espol.edu.ec>
2025-10-16 15:44:15 -0500

    Add innitials files

---
+++ Backend/index.js
@@ -0,0 +1,6 @@
+//  Develop a minimal REST API using Node.js and Express.
+// It should provide endpoints to:
+// GET /tasks — Retrieve all tasks
+// POST /tasks — Add a new task
+// PUT /tasks/:id — Mark a task as completed
+// All backend code must be developed in a single file: index.js.
